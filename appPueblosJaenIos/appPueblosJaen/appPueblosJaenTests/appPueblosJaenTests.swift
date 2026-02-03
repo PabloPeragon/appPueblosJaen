af46d98e416075f671c4831454fe63c0f1a69019
PabloPeragon <pabloperagon@gmail.com>
2026-02-03 13:59:40 +0100

    actualizacion de los test

--- appPueblosJaenIos/appPueblosJaen/appPueblosJaenTests/appPueblosJaenTests.swift
+++ appPueblosJaenIos/appPueblosJaen/appPueblosJaenTests/appPueblosJaenTests.swift
@@ -14,13 +14,16 @@ final class appPueblosJaenTests: XCTestCase {
         let id = 1
         let nombre = "Texto Nombre"
         let codigoPostal = "1234"
+        let habitantes = 10
         
-        let pueblo = Pueblo(id: 1, nombre: "Texto Nombre", codigo_postal: "1234", habitantes: nil, latitud: nil, longitud: nil, comarca: nil, descripcion: nil, url_escudo: nil, altitud: nil, superficie: nil, gentilicio: nil)
+        let pueblo = Pueblo(id: id, nombre: nombre, codigo_postal: codigoPostal, habitantes: habitantes, latitud: nil, longitud: nil, comarca: nil, descripcion: nil, url_escudo: nil, altitud: nil, superficie: nil, gentilicio: nil)
+        
         
         XCTAssertEqual(pueblo.id, id)
         XCTAssertEqual(pueblo.nombre, nombre)
         XCTAssertEqual(pueblo.codigo_postal, codigoPostal)
-        XCTAssertNil(pueblo.habitantes)
+        XCTAssertEqual(pueblo.habitantes, habitantes)
+        XCTAssertNil(pueblo.latitud)
         
     }
     
@@ -28,13 +31,15 @@ final class appPueblosJaenTests: XCTestCase {
         let id = 2
         let lugarId = 3
         let urlFoto = "Http://prueba"
+        let descripcion = "campo descripcion"
         
         
-        let puebloFoto = PuebloFoto(id: 2, lugar_id: 3, url_foto: "Http://prueba", titulo: nil, descripcion: nil, es_portada: nil, orden: nil, created_at: nil)
+        let puebloFoto = PuebloFoto(id: id, lugar_id: lugarId, url_foto: urlFoto, titulo: nil, descripcion: descripcion, es_portada: nil, orden: nil, created_at: nil)
         
         XCTAssertEqual(puebloFoto.id, id)
         XCTAssertEqual(puebloFoto.lugar_id, lugarId)
         XCTAssertEqual(puebloFoto.url_foto, urlFoto)
+        XCTAssertEqual(puebloFoto.descripcion, descripcion)
         XCTAssertNil(puebloFoto.titulo)
     }
     
@@ -43,13 +48,15 @@ final class appPueblosJaenTests: XCTestCase {
         let id = 12
         let puebloId = 10
         let nombre = "Campo nombre"
+        let tipo = "campo tipo"
         
         
-        let lugarImportante = LugarImportante(id: 12, pueblo_id: 10, nombre: "Campo nombre", tipo: nil, descripcion: nil, latitud: nil, longitud: nil, direccion: nil, horario_visita: nil, precio_entrada: nil, web: nil, telefono: nil, activo: nil, orden: nil, created_at: nil, update_at: nil)
+        let lugarImportante = LugarImportante(id: id, pueblo_id: puebloId, nombre: nombre, tipo: tipo, descripcion: nil, latitud: nil, longitud: nil, direccion: nil, horario_visita: nil, precio_entrada: nil, web: nil, telefono: nil, activo: nil, orden: nil, created_at: nil, update_at: nil)
         
         XCTAssertEqual(lugarImportante.id, id)
         XCTAssertEqual(lugarImportante.pueblo_id, puebloId)
         XCTAssertEqual(lugarImportante.nombre, nombre)
+        XCTAssertEqual(lugarImportante.tipo, tipo)
         XCTAssertNil(lugarImportante.descripcion)
     }
 }
