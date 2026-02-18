3f93979eac072f70e954308767d304796c168a95
PabloPeragon <pabloperagon@gmail.com>
2026-02-18 13:17:52 +0100

    fin de notificaciones

---
+++ appPueblosJaenIos/appPueblosJaen/appPueblosJaen/Presentation/PuebloDetail/CardNegocio/CardNegocioView.swift
@@ -0,0 +1,18 @@
+//
+//  CardNegocioView.swift
+//  appPueblosJaen
+//
+//  Created by Pablo Jesús Peragón Garrido on 18/2/26.
+//
+
+import SwiftUI
+
+struct CardNegocioView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardNegocioView()
+}
